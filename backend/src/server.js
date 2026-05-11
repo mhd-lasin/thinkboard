9b3abe93ae34545a55a38c471c5f65ad1d6975e2
mhd-lasin <lasinthangal4@gmail.com>
2026-05-11 20:20:56 +0400

    setting up the home page

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,18 @@ import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import { rateLimiter } from "./middleware/rateLimitter.js";
 
+import cors from 'cors'
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(
+  {
+    origin:"http://localhost:3000"
+  }
+
+));
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
+import NoteCard from '../components/NoteCard'
+import toast from 'react-hot-toast'
 
 const HomePage = () => {
+    const [isRateLimited, setIsRateLimited] = useState(false)
+    const [notes,setNotes] = useState([])
+    const [loading,setLoading] = useState(true)
+
+    useEffect(()=>{
+        const fetchNotes = async () => {
+            try {
+                const response = await axios.get("http://localhost:8000/api/notes")
+                console.log(response.data)
+                setNotes(response.data)
+                setIsRateLimited(false)
+            } catch (error) {
+                console.error("Error fetching notes",error)
+                if (error.response.status == 429) {
+                    setIsRateLimited(true)
+                }
+                else {
+                    toast.error("Failed to fetch notes")
+                }
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        fetchNotes()
+    },[])
     return (
-        <div>
-            <h1>Home Page</h1>
-            <p>Welcome to the Note App!</p>
+        <div className='min-h-screen'>
+            <Navbar />
+            {isRateLimited && <RateLimitedUI />}
+            <div className='max-w-6xl mx-auto p-4 mt-6'>
+                {loading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
+                {notes.length > 0 && !isRateLimited && (
+                    <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                        {notes.map((note)=>(<NoteCard key={note._id} note={note} />))}
+                    </div>
+                )}
+                
+            </div>
         </div>
     )
 }
